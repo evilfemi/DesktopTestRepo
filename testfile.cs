9235aa7aea1e551efca71b561dcb1262c0aa7e58
Femi <femisimon@github.com>
2016-10-24 18:02:08 -0700

    one more commit

--- testfile.cs
+++ testfile.cs
@@ -2,3 +2,7 @@
 ///Uptown testing -  making a change to an existing open PR to see if shows up in the list of files changed
 ///Adding another line to reproduce.
 ///Trying to resolve conflict with this line
+
+
+
+//More lines to resolve conflict. Don't think so
