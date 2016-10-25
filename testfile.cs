1a27eb28d736d3fc936a50dc321ea8cd72bb19d0
Femi Alashi <femisimon@github.com>
2016-10-24 17:58:26 -0700

    resolved conflict?

--- testfile.cs
+++ testfile.cs
@@ -1,3 +1,4 @@
 ///Commenting in the c-sharp file for testing
 ///Uptown testing -  making a change to an existing open PR to see if shows up in the list of files changed
 ///Adding another line to reproduce.
+///Trying to resolve conflict with this line
