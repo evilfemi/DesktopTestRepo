d268dc1da5ae8ebd1adb9a9a3f33e64facfc8faa
Femi Alashi <femisimon@github.com>
2016-10-24 18:30:13 -0700

    removed the additional line

--- testfile.cs
+++ testfile.cs
@@ -2,7 +2,3 @@
 ///Uptown testing -  making a change to an existing open PR to see if shows up in the list of files changed
 ///Adding another line to reproduce.
 ///Trying to resolve conflict with this line
-
-
-
-//More lines to resolve conflict. Don't think so
